6e530208fc6164f56cf5c6cbd3dfe89999482041
makiyo-ronkko <makiyo.ronkko@gmail.com>
2023-11-15 15:40:35 +0200

    main: chore: add custom background colors

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,30 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import './main.css';
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		viewMode: 'docs',
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+		backgrounds: {
+			default: 'white',
+			values: [
+				{
+					name: 'primary',
+					value: '#b3d6ff',
+				},
+				{
+					name: 'secondary',
+					value: '#54a1f8',
+				},
+			],
+		},
+	},
 };
 
 export default preview;
